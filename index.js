64b3fa14f7d15482daf5bdaf294f3472b0651369
ctyhuofc <98438791+ctyhuofc@users.noreply.github.com>
2022-02-28 16:11:42 -0700

    post/calc seperation

--- index.js
+++ index.js
@@ -5,39 +5,51 @@ const myModule = require("./arithmeticFunctions.js");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req,res){ //perform a get function using express through "app"
+app.get("/calc", function(req,res){ //perform a get function using express through "app"
     res.sendFile(__dirname + "/index.html")     //send index.html as a response
 });
 
-app.post("/", function(req, res){
-    
+app.get("/", function(req,res){ //perform a get function using express through "app"
+    res.send("<script>console.log('worked')</script>")     //send index.html as a response
+});
 
+app.get("/post", function(req,res){
+    res.sendFile(__dirname + "/hello.html")
+});
+
+app.post("/post", function(req,res){
+    res.send("Check Terminal")
+    
     function calculate(oper, value1,value2){ //console function
 
 
-    if(oper==='add'){
-        answer = myModule.add(value1, value2)
-        console.log('Operation: Addition Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
-    }else if(oper==='sub'){
-        answer = myModule.sub(value1, value2)
-        console.log('Operation: Subtraction Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
-    }else if(oper==='mul'){
-        answer = myModule.multiply(value1, value2)
-        console.log('Operation: Multiplication Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
-    }else if(oper==='div'){
-        answer = myModule.divide(value1, value2)
-        console.log('Operation: Division Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
-    }
-    }
+        if(oper==='add'){
+            answer = myModule.add(value1, value2)
+            console.log('Operation: Addition Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
+        }else if(oper==='sub'){
+            answer = myModule.sub(value1, value2)
+            console.log('Operation: Subtraction Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
+        }else if(oper==='mul'){
+            answer = myModule.multiply(value1, value2)
+            console.log('Operation: Multiplication Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
+        }else if(oper==='div'){
+            answer = myModule.divide(value1, value2)
+            console.log('Operation: Division Value 1: ' +value1+ ' Value 2: ' +value2+ ' Result: ' +answer)
+        }
+        }
+        calculate('div', 5,3) //console arguments + answer
+});
 
-    console.log(calculate('add', 5,3)) // console Answer
+app.post("/", function(req, res){ //post responses
 
-    const value1 = Number(req.body.num1); //html functions
+
+    
+    const value1 = Number(req.body.num1); //html variables
     const value2 = Number(req.body.num2);
     const oper = req.body.operation;
 
 
-    if(oper==='add'){
+    if(oper==='add'){                      //html conditionals
         answer = myModule.add(value1, value2)
         res.send('Operation: Addition<br>Value 1: ' +value1+ '<br>Value 2: ' +value2+ '<br>Result: ' +answer)
     }else if(oper==='sub'){
@@ -50,12 +62,15 @@ app.post("/", function(req, res){
         answer = myModule.divide(value1, value2)
         res.send('Operation: Division<br>Value 1: ' +value1+ '<br>Value 2: ' +value2+ '<br>Result: ' +answer)
     }
+
 });
+
 
 app.listen(3000, function(){
     console.log("at port 3000 Server is ON")
     
 }); // Open server on port 3000 using express/app
+
 
 
 
